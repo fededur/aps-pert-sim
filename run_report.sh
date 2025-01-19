908b9f01e8461f502c8867990c96680caab3e41a
fededur <143073692+fededur@users.noreply.github.com>
2025-01-20 09:57:17 +1300

    path issue

--- run_report.sh
+++ run_report.sh
@@ -6,10 +6,11 @@ IMAGE_NAME="aps_pert_sim_image"
 # Build the Docker image
 docker build -t $IMAGE_NAME .
 
-# Run the Docker container with mounted directory and set working directory
-docker run --rm -v "$(pwd):/project" -w /project $IMAGE_NAME
+# Run the Docker container with corrected path handling for Windows
+docker run --rm -v "$(pwd -W):/project" -w "//project" $IMAGE_NAME
 
 # Clean up Docker image
 docker rmi $IMAGE_NAME
 
 echo "Report generation complete! Check the 'output' directory."
+
